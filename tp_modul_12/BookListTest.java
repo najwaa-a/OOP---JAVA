3701653fb2cb63b52cae1c190d388fdcb2d44221
Najwa <najwaareefa@gmail.com>
2025-05-13 23:59:19 +0700

    Update BookListTest.java

--- tp_modul_12/BookListTest.java
+++ tp_modul_12/BookListTest.java
@@ -1,4 +1,4 @@
-/*
+n/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -13,12 +13,22 @@ import java.util.ArrayList;
 public class BookListTest {
     public static void main(String[] args) {
        
-        ArrayList<Book> book = new ArrayList<Book>();
+        ArrayList<Book> books = new ArrayList<Book>();
         
-        book.add(new Book("Rules of Java", 346, 15000));
-        book.add(new Book("Oracle 11g", 560, 18000));
-        book.add(new Book("Copying JSP", 271, 12000));
+        books.add(new Book("Rules of Java", 346, 15000));
+        books.add(new Book("Oracle 11g", 560, 18000));
+        books.add(new Book("Copying JSP", 271, 12000));
         
-        System.out.print(book);
+       for (int inx = 0; inx < books.size(); inx++){
+            Book book = books.get(inx);
+            System.out.print(book.getJudulBuku());
+            System.out.print("(");
+            System.out.print(book.getJumlahHalaman());
+            System.out.print(" halaman)");
+            System.out.print(":");
+            System.out.print(book.getHarga());
+            System.out.print(" won");
+            System.out.println("  ");
+        }
     }
 }
