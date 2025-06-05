45d3b1000eab3bbeb980123811ac0738baefad62
Najwa <najwaareefa@gmail.com>
2025-06-05 21:50:11 +0700

    Add files via upload

---
+++ hotel/Hotel.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel;
+
+/**
+ *
+ * @author HP
+ */
+public class Hotel {
+    private String namaHotel;
+    private String lokasi;
+    
+    Hotel(String nama, String lokasi){
+        this.namaHotel = namaHotel;
+        this.lokasi = lokasi;
+    }
+    
+    public void tambahKamar(){
+        return ;
+    }
+    
+    public void tampilkanSemuaKamar(){
+        return;
+    }
+    
+    
+    
+    
+}
